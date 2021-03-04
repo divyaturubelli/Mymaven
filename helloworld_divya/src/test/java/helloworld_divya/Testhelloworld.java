ec19f0f618879932782067b3c0fad931e9bf0d8b
leadaws <leadaws@gmail.com>
2021-03-04 19:57:02 +0530

    client

---
+++ helloworld_divya/src/test/java/helloworld_divya/Testhelloworld.java
@@ -0,0 +1,11 @@
+package helloworld_divya;
+
+import org.testng.annotations.Test;
+
+public class Testhelloworld {
+	
+	@Test
+public void testhelloworld() {
+	System.out.println("..hello world...");
+}
+}
